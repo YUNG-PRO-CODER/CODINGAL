69cb2ddd13f37c762f9dc8d0cd86bbd394eaedb8
YUNG-PRO-CODER <atharvsaini17@gmail.com>
2026-08-06 23:50:37 +0530

    :

--- Codingal/Data structure I/Math magic I/main.py
+++ Codingal/Data structure I/Math magic I/main.py
@@ -1,4 +1,3 @@
-
 """armstrong number"""
 
 try:
@@ -21,4 +20,20 @@ try:
 except ValueError:
     print("wrong input try again") 
     
-""""""
+"""factors"""
+
+
+try:
+        
+    
+    def print_factors(num):
+            
+            for i in range(1, num + 1):
+                if num % i == 0:
+                    result = num // i
+                    print(result)
+    print_factors(20)        
+            
+except ValueError:
+    print("try again")
+
--- Codingal/Data structure I/Math magic I/tempCodeRunnerFile.py
+++ Codingal/Data structure I/Math magic I/tempCodeRunnerFile.py
@@ -1,17 +1,13 @@
 try:
-    num = int(input("Type a number "))
-    result = 0
-    digit = len(str(num))
-
-    t = num
-    while t > 0:
-        digits = num % 10
-        result = digits ** digit
-        t // 10
         
-    if num == result:
-        print("Its a armstrong number ", num) 
-    else:
-        print("its not")  
+    
+    def print_factors(num):
+            
+            for i in range(1, num + 1):
+                if num % i == 0:
+                    result = num // i
+                    print(result)
+    print_factors(20)        
+            
 except ValueError:
-    print("wrong input try again") 
+    print("try again")
